757dc1926899eccbf3733985f638fd4c2ab4f535
philip-white <phlwhite5@gmail.com>
2020-07-18 21:36:17 -0300

    modified:   app/Http/Controllers/PagesController.php 	modified:   resources/views/pages/findMe.blade.php

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -33,15 +33,27 @@ class PagesController extends Controller
         $playerSearch = $_POST['playerSearch'];
 
 
-
+        //general player data
         $player = new Player($playerSearch);
         $player = $player->get();
         $playerData = json_decode($player);
 
+        //player's next/"upcoming" chests
+        $playerChest = new Player($playerSearch);
+        $upcomingChests = $playerChest->upcomingChests();
+        $chests = json_decode($upcomingChests);
+
+        //player's battles info
+        $playerBattles = new Player($playerSearch);
+        $battles = $playerBattles->battles();
+        $battlesData = json_decode($battles);
+
+
+
         
         
         
-        return view('pages.findMe')->with('playerData', $playerData);
+        return view('pages.findMe',['playerData' => $playerData, 'chests' => $chests, 'battlesData' => $battlesData]);
         
 
     }
--- resources/views/pages/findMe.blade.php
+++ resources/views/pages/findMe.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 <h1>{{$playerData->name}}</h1>
 <h1>{{$playerData->clan->name}}</h1>
+<h1>{{$playerData->clan->tag}}
 <h1>{{$playerData->role}}</h1>
 <h1>{{$playerData->wins}}</h1>
 <h1>{{$playerData->losses}}</h1>
@@ -11,6 +12,12 @@
 <h1>{{$playerData->warDayWins}}</h1>
 
 <h1>{{$playerData->battleCount}}</h1>
+<!--this is to find past battle data for a player-->
+<h1>{{$battlesData[0]->type}}</h1>
 
+<!--this is to find the next chests that the player will obtain
+    in the game -->
+
+<h1>{{$chests->items[0]->name}}</h1>
 
 @endsection
